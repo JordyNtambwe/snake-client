4747b6aca7460d89bcc95eaa67dabca862f407e3
JordyNtambwe <jordyntambwe@gmail.com>
2024-03-10 22:22:48 -0600

    commit on master

--- client.js
+++ client.js
@@ -1,9 +1,10 @@
 const net = require("net");
 
+// establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
     host: "localhost",
-    port: 50541
+    port: 50541,
   });
 
   conn.on("data", (data) => {
@@ -12,6 +13,10 @@ const connect = function () {
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    conn.write("Name: JN")
+    console.log("Successfully connected to game server")
+  })
 
   return conn;
 
