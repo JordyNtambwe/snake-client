204fbfc5853e3984fcc08b7b1e1b90bdf2eb1c2d
JordyNtambwe <jordyntambwe@gmail.com>
2024-03-13 19:20:33 -0600

    commit on master

--- input.js
+++ input.js
@@ -1,17 +1,32 @@
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
+let connection;
 
-  const handleUserInput = function (key) {
+const setupInput = function (conn) {
+  connection = conn;
+  if(key === w) {
+    console.log("Move: up")
+  }
+  if(key === a) {
+    console.log("Move: left")
+  }
+  if(key === s) {
+    console.log("Move: down")
+  }
+  if(key === d) {
+    console.log("Move: right")
+  }
+}
+
+const stdin = process.stdin;
+stdin.setRawMode(true);
+stdin.setEncoding("utf8");
+stdin.resume();
+
+const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
-  }
-};
+   }
+
+}
+
   stdin.on("data", handleUserInput);
   return stdin;
-
-};
-
-module.exports = setupInput
